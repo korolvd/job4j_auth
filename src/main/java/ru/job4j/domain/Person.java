fbd4d95ce9261ccc8e268556c49efa8e0c02ca44
korolvd <ffirin@mail.ru>
2022-05-03 17:43:55 +0500

    0. RESTFull. Описание архитектуры [#6884]

---
+++ src/main/java/ru/job4j/domain/Person.java
@@ -0,0 +1,39 @@
+package ru.job4j.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String login;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
